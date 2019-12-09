Fixed major bug in SNR calculation.
SNR was being heavily underestimated when large outliers
were present.

bls_tools.py
@@ -197,6 +197,13 @@ def search_transits(t, f, num_searches=10, R_star=0.1, M_star=0.1,
         bls_peaks.at[i, 'power'] = power
         bls_peaks.at[i, 'noise'] = bls_noise
         bls_peaks.at[i, 'snr'] = blss.loc[blss.period == period, 'snr'].iloc[0]
+
+        # BUG TODO
+        if not np.isfinite(bls_peaks.loc[i, 'snr']):
+            print("Non-finite snr in bls_peaks:", bls_peaks)
+            raise ValueError("bls_peaks snr is nan. i = {}\n"
+                             "bls_peaks = {}".format(i, bls_peaks))
+
         assert np.isfinite(bls_peaks.loc[i, 'snr'])
 
         i += 1
@@ -621,20 +628,27 @@ def get_split_info(P_min, P_max, T, R_star, M_star,
             # the loop and leave si as it is
             #
             # BUG
-            print("Breaking split info, p_min", p_min)
-            si.drop(i, axis='index')
+            print(" f_min inversion.\n", '-'*40)
+            print(" ".join(["Inversion error,",
+                            "P_min = {}, P_max = {},".format(p_min, p_max),
+                            "fmin = {},".format(si.at[i, 'fmin']),
+                            "fmax = {}".format(si.at[i, 'fmax']),
+                            "T = {}".format(T)]))
+
+            si.drop(i, axis='index', inplace=True)
             break
 
-            # BUG: flagging
-            pd.set_option('display.max_columns', 30)
-            pd.set_option('display.max_rows', len(si))
-            err_str = " ".join(["Inversion error,",
-                                "P_min = {}, P_max = {},".format(p_min, p_max),
-                                "fmin = {},".format(si.at[i, 'fmin']),
-                                "fmax = {}".format(si.at[i, 'fmax']),
-                                "T = {}".format(T)])
-            print(err_str)
-            raise InvertedLimitsError(err_str, si)
+            # BUG: flagging (will never get here)
+            # TODO: This part is obsolete.
+            # pd.set_option('display.max_columns', 30)
+            # pd.set_option('display.max_rows', len(si))
+            # err_str = " ".join(["Inversion error,",
+            #                     "P_min = {}, P_max = {},".format(p_min, p_max),
+            #                     "fmin = {},".format(si.at[i, 'fmin']),
+            #                     "fmax = {}".format(si.at[i, 'fmax']),
+            #                     "T = {}".format(T)])
+            # print(err_str)
+            # raise InvertedLimitsError(err_str, si)
 
         si.at[i, 'nf'] = get_nf(P=p_max,
                                 R_star=R_star,

tf_tools/tf_tools.py
@@ -20,7 +20,7 @@ from ..__init__ import HOME_DIR
 # ------------------------
 
 def fit_transits(t, f, bls_peaks, R_star, M_star, bin_type='regular',
-                 bin_res=8, calc_snr=True, subtract_results=False,
+                 bin_res=8, calc_snr=False, subtract_results=False,
                  freeze_a=True, overlap_lim='full', **fit_kwargs):
     """Tries to fit all the peaks in 'bls_peaks' with batman.
 
@@ -42,8 +42,8 @@ def fit_transits(t, f, bls_peaks, R_star, M_star, bin_type='regular',
         subtract_results (bool): if True, subtracts the fitted
             transits before fitting the next
         **fit_kwargs: additional params to `fit_single_transit`,
-            e.g. cut_lightcurve, iterations, burn, nwalkers,
-                 adjust_res
+            e.g. cut_lightcurve, iterations, burn,
+                 nwalkers, adjust_res, f_err
 
     Returns:
         bls_peaks but updated with the fitted parameters, under
@@ -86,9 +86,7 @@ def fit_transits(t, f, bls_peaks, R_star, M_star, bin_type='regular',
         if p_initial.isnull().any():
             raise ValueError('p_initial contained NaN.')
 
-        # BUG
-        try:
-            p_fit, _ = fit_single_transit(t, f, bin_type=bin_type,
+        p_fit, _ = fit_single_transit(t, f, bin_type=bin_type,
                                       bin_res=bin_res,
                                       freeze_a=freeze_a,
                                       overlap_lim=overlap_lim,
@@ -96,11 +94,6 @@ def fit_transits(t, f, bls_peaks, R_star, M_star, bin_type='regular',
                                       M_star=M_star,
                                       **fit_kwargs,
                                       **p_initial)
-        except ValueError as e:
-            print("In fit_transits, prior error calling fit_single_transit")
-            print("**p_initial\n", p_initial)
-            print("**fit_kwargs\n", fit_kwargs)
-            raise e from None
 
         if calc_snr:
             _, _, p_fit['snr_fit'], p_fit['mcmc_flag'] = sample_transit(
@@ -132,7 +125,7 @@ def fit_transits(t, f, bls_peaks, R_star, M_star, bin_type='regular',
     return bls_peaks
 
 def fit_single_transit(t, f, bin_type='regular', bin_res=6,
-                       cut_lightcurve=True,
+                       cut_lightcurve=True, f_err=None,
                        adjust_res=True, freeze_a=True,
                        overlap_lim='full', **fit_params):
     """Fits a single transit from bls parameters.
@@ -161,7 +154,7 @@ def fit_single_transit(t, f, bin_type='regular', bin_res=6,
             M_star, R_star, BLS-PARAMS
 
     Returns:
-        p_fit, result_df, chain_df, params
+        p_fit, batman.params
     """
 
     mcmc_keys = ('iterations', 'burn', 'nwalkers', 'plot_posterior')
@@ -174,7 +167,8 @@ def fit_single_transit(t, f, bin_type='regular', bin_res=6,
     if abs(np.nanmedian(f)) > 0.1:
         f = f + 1.0 - np.nanmedian(f)
 
-    f_err = stats.sigmaclip(f)[0].std()
+    if f_err is None:
+        f_err = stats.sigmaclip(f)[0].std()
 
     # BUG
     try:

tf_tools/transit_fitter.py
@@ -342,8 +342,7 @@ class TransitFitter(TransitModel):
 
         return model_lnlikelihood - null_lnlikelihood
 
-    def estimate_snr(self, count_points=True, duration_window=0.9,
-                     params=None, subtract_transit=True):
+    def estimate_snr(self, count_points=True,  subtract_transit=True):
         """Estimate the SNR for a basic gaussian assumption.
 
         Args:
@@ -370,11 +369,14 @@ class TransitFitter(TransitModel):
             return 0.0
 
         if subtract_transit:
-            f_lcf = self.f_data - self.evaluate_model()
+            f_lcf = self.f_data - self.evaluate_model() + f0
         else:
             f_lcf = self.f_data.copy()
 
-        signoise = np.nanstd(f_lcf)
+        # NOTE: cannot calculate noise directly; megaflares, such as
+        # those in trappist-1, completely mess this up. Instead, need
+        # to use the GP noise, or sigma-clip the noise out.
+        #signoise = np.nanstd(f_lcf)
 
         if count_points:
             npoints = np.sum(t_mask)
@@ -383,7 +385,7 @@ class TransitFitter(TransitModel):
             t_base = np.nanmax(self.t_data) - np.nanmin(self.t_data)
             npoints = t_base * adj_dur / (self['per'] * t_cad)
 
-        return depth * np.sqrt(npoints) / signoise
+        return depth * np.sqrt(npoints) / self.f_err
 
     # Optimization and process methods
     # --------------------------------
@@ -484,9 +486,15 @@ class TransitFitter(TransitModel):
         p0 = p_0 + 1e-6*np.random.randn(nwalkers, ndim)
 
         # Sampling procedure
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, self.lnposterior)
-        sampler.run_mcmc(p0, iterations + burn)
-        chain = sampler.flatchain[burn:]
+        sampler = emcee.EnsembleSampler(nwalkers=nwalkers,
+                                        dim=ndim,
+                                        lnpostfn=self.lnposterior)
+
+        pos, _, _ = sampler.run_mcmc(p0, burn)
+        sampler.reset()
+        sampler.run_mcmc(pos, iterations)
+
+        chain = sampler.flatchain
 
         # NOTE: SNR currently doesn't tell us if the radius converged
         # to the right value or at all (it could converge to 0 and

tf_tools/transit_model.py
@@ -626,7 +626,7 @@ class TransitModel(object):
 	def calc_a(self, R_star, M_star, per=None, set_value=False):
 		per = self['per'] if per is None else per
 	
-		M_fac = (0.5*per*units.day/np.pi)**(2/3) * (0.5*const.G)**(1/3)
+		M_fac = (0.5*per*units.day/np.pi)**(2/3) * const.G**(1/3)
 		a = (M_fac*(M_star*const.M_sun)**(1/3) / (R_star*const.R_sun)).to('').value
 
 		if set_value: